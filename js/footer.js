16eb3856522846509b7d1652c2f53b7ba364d714
Stephane Joly <102809026+Scarecrow1965@users.noreply.github.com>
2023-10-03 12:49:11 -0500

    Add files via upload
    Change 2023-10-03

--- js/footer.js
+++ js/footer.js
@@ -1,9 +1,25 @@
+// Declare and store the data into a variable
+// const currDate = Date.now();
+const currentDate = new Date();
+
+// Prints the UNIX epoch
+// console.log("Unix time stamp of current date", currDate);
+console.log("Unix time stamp of current date", currentDate);
+
+// Converts timestamp into Date Object
+// const dt = new Date(currDate);
+const currentYear = currentDate.getFullYear();
+
+// Print the full year
+// console.log(dt.getFullYear());
+console.log(currentYear);
+
 const template2 = document.createElement("template");
 template2.innerHTML = `
     <footer class="footer">
         <div class="container">
             <div class="footer__top">
-                <div class="row">
+               <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-4">
                         <div class="footer__top__logo">
                             <a href="index.html"><img src="img/logo.png" alt=""></a>
@@ -21,9 +37,8 @@ template2.innerHTML = `
                 <div class="row">
                     <div class="col-lg-12 text-center">
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        <p class="footer__copyright__text">Copyright &copy;&nbsp;2021- 
-                            <script>document.write(new Date().getFullYear());</script>
-                            &nbsp;All rights reserved | <a href="index.html" rel="noopener noreferrer" target="_blank">Cyber Catalyst</a>
+                        <p class="footer__copyright__text">
+                            Copyright &copy;&nbsp;2021-${currentYear}&nbsp;All rights reserved | <a href="index.html" rel="noopener noreferrer">Cyber Catalyst</a>
                         </p>
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                     </div>
@@ -33,15 +48,3 @@ template2.innerHTML = `
     </footer>
 `;
 document.body.appendChild(template2.content);
-
-// Declare and store the data into a variable
-const currDate = Date.now();
-
-// Prints the UNIX epoch
-console.log("Unix time stamp of current date", currDate);
-
-// Converts timestamp into Date Object
-const dt = new Date(currDate)
-
-// Print the full year
-console.log(dt.getFullYear())
